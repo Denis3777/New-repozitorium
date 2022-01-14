6cd1e1f2b5c6757756d520822f7fe706877d915c
Din3777 <denis.hryszko@gmail.com>
2022-01-14 14:49:05 +0100

    feat: solve the task

---
+++ index.js
@@ -0,0 +1,5 @@
+<form>
+<SELECT NAME="sel" onchange="top.location.href = this.options[this.selectedIndex].value;">
+<optionLUE= selected value="#">Ваш выбор</optionLUE="http://kdg.HtmlWeb.ru">Home Page Author</LUE=>
+<option VALUE="http://www.webclub.ru/">Клуб WEBмастеров</option>
+<option VALUE="http://www.citforum.ru/">Море аналитической информации</option></select></form>
